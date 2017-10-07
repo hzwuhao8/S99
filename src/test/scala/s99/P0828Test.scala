2fdfa11c0740966a87aa42d932bc1c3e37988516
wuhao <wuhao@bokee.net>
2017-10-08 07:34:46 +0800

    Checkers  support  check( Prop )

--- src/test/scala/s99/P0828Test.scala
+++ src/test/scala/s99/P0828Test.scala
@@ -22,28 +22,17 @@ class P0828Test extends FunSuite with Checkers {
     assert(res1 == Nil)
   }
   test("P08 check") {
-    val smallString = Gen.listOfN[Char](10, Gen.alphaChar).map { l =>
-      val l1 = l.distinct
-
-      val l2 = l1.zipWithIndex.map {
-        case (c, index) =>
-
-          (0 to (scala.util.Random.nextInt(index) + 1)).map(c => c)
-      }
-      (l1, l2.flatten)
+    val smallIntGen = Gen.choose(0, 100)
+    val l0gen = smallIntGen.flatMap { max => Gen.listOfN(max, Gen.alphaNumChar) }
+    val l1gen = l0gen.map { _.distinct }
+    val resgen: Gen[Boolean] = l1gen.map { l1 =>
+      val l2 = l1.map { c => (0 to (scala.util.Random.nextInt(10) + 1)).map(_ => c) }
+      //println(s"l1@gen=${l1}")
+      //println(s"l2@gen=${l2}")
+      l1 == compress(l2.flatten)
     }
 
-    check { (size: Int) =>
-      val smallIntGen = Gen.choose(0, 100)
-      val l0gen = smallIntGen.flatMap { max => Gen.listOfN(max, Gen.alphaNumChar) }
-      val l1gen = l0gen.map { _.distinct }
-      val resgen: Gen[Boolean] = l1gen.map { l1 =>
-        val l2 = l1.map { c => (0 to (scala.util.Random.nextInt(10) + 1)).map(_ => c) }
-        //println(s"l1@gen=${l1}")
-        //println(s"l2@gen=${l2}")
-
-        l1 == compress(l2.flatten)
-      }
+    check {
       Prop.forAll(resgen) { x => x }
     }
 
