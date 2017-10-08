b9cc515f8f591f41ee43947960735b23773bb8f4
wuhao <wuhao@bokee.net>
2017-10-08 08:25:07 +0800

    P08  compress2  tailrec

--- src/main/scala/s99/P0828.scala
+++ src/main/scala/s99/P0828.scala
@@ -11,4 +11,15 @@ object P0828 {
 
   }
 
+  def compress2[A](xs: List[A]): List[A] = compressRec(xs, Nil)
+
+  @scala.annotation.tailrec
+  def compressRec[A](xs: List[A], res: List[A]): List[A] = {
+    xs match {
+      case Nil                          => res
+      case y1 :: Nil                    => res ::: List(y1)
+      case y1 :: y2 :: ys if (y1 == y2) => compressRec(y1 :: ys, res)
+      case y1 :: y2 :: ys               => compressRec(y2 :: ys, res ::: List(y1))
+    }
+  }
 }
--- src/test/scala/s99/P0828Test.scala
+++ src/test/scala/s99/P0828Test.scala
@@ -14,12 +14,19 @@ import org.scalacheck.Prop
 class P0828Test extends FunSuite with Checkers {
   import P0828._
   test("P08 Eliminate consecutive duplicates of list elements.") {
-    val res1 = compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
-    assert(res1 == List('a, 'b, 'c, 'a, 'd, 'e))
+    val l1 = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+    val l2 = List('a, 'b, 'c, 'a, 'd, 'e)
+    val res1 = compress(l1)
+    assert(res1 == l2)
+    val res2 = compress2(l1)
+    assert(res2 == l2)
+
   }
   test("P08 compress empty List ") {
     val res1 = compress(Nil)
     assert(res1 == Nil)
+    val res2 = compress2(Nil)
+    assert(res2 == Nil)
   }
   test("P08 check") {
     val smallIntGen = Gen.choose(0, 100)
@@ -29,13 +36,18 @@ class P0828Test extends FunSuite with Checkers {
       val l2 = l1.map { c => (0 to (scala.util.Random.nextInt(10) + 1)).map(_ => c) }
       //println(s"l1@gen=${l1}")
       //println(s"l2@gen=${l2}")
-      l1 == compress(l2.flatten)
+      val l3 = l2.flatten
+      l1 == compress(l3) && l1 == compress2(l3)
+
     }
 
     check {
       Prop.forAll(resgen) { x => x }
     }
 
+  }
+  test("P08  compress == compress2 ") {
+    check { (a: List[AnyVal]) => compress(a) == compress2(a) }
   }
 
 }
