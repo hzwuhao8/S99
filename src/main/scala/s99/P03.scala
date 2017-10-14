539a61c074db2aa14ed231e7f80e370bdd2192a6
wuhao <wuhao@bokee.net>
2017-10-14 18:41:02 +0800

    nth;  0 <= i < list.size

--- src/main/scala/s99/P03.scala
+++ src/main/scala/s99/P03.scala
@@ -6,7 +6,7 @@ object P03 {
   @tailrec
   def nth[A](xs: List[A], index: Int): A = (xs, index) match {
     case (Nil, _)        => throw new java.lang.IndexOutOfBoundsException(index.toString)
-    case (_, i) if i < 0 => throw new java.lang.IndexOutOfBoundsException(index.toString)
+    case (xs, i) if i < 0 || i >=xs.size => throw new java.lang.IndexOutOfBoundsException(index.toString)
     case (y :: ys, 0)    => y
     case (y :: ys, i)    => nth(ys, i - 1)
 
