d77b15a62e4f3624ad6f3827edc8c9bcf65e8688
wuhao <wuhao@bokee.net>
2017-10-15 20:27:38 +0800

    need more learn BNF

--- src/main/scala/s99/MTree.scala
+++ src/main/scala/s99/MTree.scala
@@ -39,34 +39,48 @@ object MTree extends Log {
   }
 }
 
+/**
+ * *
+ *  Value = [a-z]
+ *  T = v |
+ *  s = v (v^)*  tt^t^...
+ *  s = v(v(v^)^) => ttt^^
+ *  ab^c^d^ = a(b,c,d)
+ *  ab^cd^^ = a(b,(c(d)))
+ *  cd^^ => (c(d))^
+ *  d^ =(d)
+ *
+ */
 object MTreeParse {
   import fastparse.all._
   val cc: P[Char] = P(CharIn('a' to 'z').!.map(_.head))
-  val baser1: P[MTree[Char]] = P { cc }.map { c => MTree(c) }
+  val baser1: P[MTree[Char]] = P { cc ~ fastparse.all.End }.log().map { c => MTree(c) }
 
-  val baser2: P[MTree[Char]] = P { baser1 ~ (baser1 ~ "^").rep(1) }.map { p => MTree(p._1.value, p._2.toList) }
+  val baser2: P[MTree[Char]] = P { cc ~ (cc ~ "^").rep(1)  }.log().map { p => MTree(p._1, p._2.toList.map { MTree(_) }) }
 
-  val baser3: P[MTree[Char]] = P { baser1 ~ (baser1 ~ "^").rep ~ baser1 ~ baser1 ~ "^^" }.map { p =>
-    val parent = p._1
-    val child = p._2.toList
-    val lastchild = p._3.copy(children = List(p._4))
-    parent.copy(children = (child ::: List(lastchild)))
-  }
+  //  val baser3: P[MTree[Char]] = P { cc ~ (cc ~ "^").rep ~ cc ~ cc ~ "^^" }.log().map { p =>
+  //    val parent = p._1
+  //    val child = p._2.toList.map { MTree(_) }
+  //    val lastchild = MTree(p._3, List(MTree(p._4)))
+  //    MTree(parent, (child ::: List(lastchild)))
+  //  }
 
   //val r1: P[MTree[Char]] = P { rr ~ rr ~ "^" }.map { p => MTree(p._1.value, List(p._2)) }
 
-  val r2a: P[Seq[MTree[Char]]] = P { (rr ~ "^").rep(1) }
+  //  val r2a: P[Seq[MTree[Char]]] = P { (rr ~ "^").rep(1) }.log()
 
-  val r2: P[MTree[Char]] = P { rr ~ r2a }.map { p => p._1.copy(children = p._2.toList) }
+  val r2: P[MTree[Char]] = P { rr ~ (rr ~ "^").rep(1) }.log().map { p => p._1.copy(children = p._1.children ::: p._2.toList) }
 
-  val r3: P[MTree[Char]] = P { rr ~ (rr ~ "^").rep(0) ~ rr ~ rr ~ "^^" }.map { p =>
-    val parent = p._1
-    val child = p._2.toList
-    val lastchild = p._3.copy(children = List(p._4))
-    parent.copy(children = (child ::: List(lastchild)))
-  }
+  val r2a: P[MTree[Char]] = P { cc ~ (rr ~ "^").rep(1) }.log().map { p => MTree(p._1, children = p._2.toList) }
 
-  val rr: P[MTree[Char]] = P { baser3 | baser2 | baser1 | r3 | r2 }
+  //  val r3: P[MTree[Char]] = P { cc ~ (rr ~ "^").rep(0) ~cc~ r2 ~ "^" }.log().map { p =>
+  //    val parent = p._1
+  //    val child = p._2.toList
+  //    val lastchild = MTree(p._3,List( p._4))
+  //    MTree(parent, children = (child ::: List(lastchild)))
+  //  }
+
+  val rr: P[MTree[Char]] = P { baser2 | baser1 | r2a | r2 }.log()
 
   val rrr = P { rr ~ fastparse.all.End }
 
