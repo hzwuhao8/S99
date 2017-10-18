7c53db16270ed87b8b4d9755825482dee71c9097
wuhao <wuhao@bokee.net>
2017-10-18 20:00:20 +0800

    function  term and  adjacent

---
+++ src/main/scala/s99/Graph.scala
@@ -0,0 +1,92 @@
+package s99
+
+abstract class GraphBase[T, U] {
+  case class Edge(n1: Node, n2: Node, value: U) {
+    def toTuple = (n1.value, n2.value, value)
+  }
+  case class Node(value: T) {
+    var adj: List[Edge] = Nil
+    def neighbors: List[Node] = adj.map(edgeTarget(_, this).get)
+  }
+
+  var nodes: Map[T, Node] = Map()
+  var edges: List[Edge] = Nil
+
+  // If the edge E connects N to another node, returns the other node,
+  // otherwise returns None.
+  def edgeTarget(e: Edge, n: Node): Option[Node]
+
+  override def equals(o: Any) = o match {
+    case g: GraphBase[T, U] => ((nodes.keys.toSet -- g.nodes.keys.toSet).isEmpty &&
+      (edges.map(_.toTuple).toSet -- g.edges.map(_.toTuple).toSet).isEmpty)
+    case _ => false
+  }
+
+  def addNode(value: T) = {
+    val n = new Node(value)
+    nodes = Map(value -> n) ++ nodes
+    n
+  }
+}
+
+class Graph[T, U] extends GraphBase[T, U] {
+  override def equals(o: Any) = o match {
+    case g: Graph[T, U] => super.equals(g)
+    case _              => false
+  }
+
+  def edgeTarget(e: Edge, n: Node): Option[Node] =
+    if (e.n1 == n) Some(e.n2)
+    else if (e.n2 == n) Some(e.n1)
+    else None
+
+  def addEdge(n1: T, n2: T, value: U) = {
+    val e = new Edge(nodes(n1), nodes(n2), value)
+    edges = e :: edges
+    nodes(n1).adj = e :: nodes(n1).adj
+    nodes(n2).adj = e :: nodes(n2).adj
+  }
+}
+
+object Graph {
+  def term[T](nodeList: List[T], edgeList: List[(T, T)]) = {
+    val g = new Graph[T, Int]
+    nodeList.map { n => g.addNode(n) }
+    nodeList.map { n =>
+      {
+        val subList = edgeList.filter(_._1 == n)
+        subList.foreach(x => g.addEdge(x._1, x._2, 0))
+      }
+    }
+    g
+  }
+
+  def adjacent[T](edgesList: List[(T, List[T])]) = {
+    val g = new Graph[T, Int]
+    edgesList.foreach { e => g.addNode(e._1) }
+    edgesList.map { e =>
+      g.addNode(e._1)
+      e._2.foreach { n =>
+        g.addEdge(e._1, n, 0)
+      }
+    }
+    g
+  }
+}
+
+class Digraph[T, U] extends GraphBase[T, U] {
+  override def equals(o: Any) = o match {
+    case g: Digraph[T, U] => super.equals(g)
+    case _                => false
+  }
+
+  def edgeTarget(e: Edge, n: Node): Option[Node] =
+    if (e.n1 == n) Some(e.n2)
+    else None
+
+  def addArc(source: T, dest: T, value: U) = {
+    val e = new Edge(nodes(source), nodes(dest), value)
+    edges = e :: edges
+    nodes(source).adj = e :: nodes(source).adj
+  }
+}
---
+++ src/test/scala/s99/GraphTest.scala
@@ -0,0 +1,25 @@
+package s99
+
+import org.scalatest.FunSuite
+import org.scalatest.prop.Checkers
+import org.scalacheck.Arbitrary._
+import org.scalacheck.Prop._
+import org.scalacheck.Gen
+import org.scalacheck.Prop
+
+class GraphTest extends FunSuite with Checkers with Log {
+
+  test("") {
+    val g1 = Graph.term(List('b', 'c', 'd', 'f', 'g', 'h', 'k'),
+      List(('b', 'c'), ('b', 'f'), ('c', 'f'), ('f', 'k'), ('g', 'h')))
+
+    debug(s"g1=${g1}")
+    val g2 = Graph.adjacent(List(('b', List('c', 'f')), ('c', List('b', 'f')), ('d', Nil),
+      ('f', List('b', 'c', 'k')), ('g', List('h')), ('h', List('g')),
+      ('k', List('f'))))
+
+    debug(s"g2=${g2}")
+
+    assert(g1 == g2)
+  }
+}
